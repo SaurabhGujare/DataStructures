37b291cb4a3cbed5cb91f7c73a00fd4475f3b1af
SAURABH NITIN GUJARE <gujare.s@husky.neu.edu>
2019-05-10 13:51:07 -0400

    Adding StringManipulation Project

---
+++ StringManipulation/src/StringCompare.java
@@ -0,0 +1,29 @@
+public class StringCompare {
+
+    public static void main(String[] args){
+        String s1="Sachin";
+        String s2="SACHIN";
+        String s3="Sachin";
+        String s4 = new String("Sachin");
+        String s5 = "Ratan";
+        String s6="SACHINTYUTIUY";
+
+        System.out.println("s1=Sachin\ns2=SACHIN\ns3=Sachin\ns4= new String(Sachin)\ns5=Ratan\nString s6=SACHINTYUTIUY\n\n");
+        System.out.println("s1.eqauls(s2): "+s1.equals(s2));
+        System.out.println("s1.equalsIgnoreCase(s2): "+s1.equalsIgnoreCase(s2));
+        System.out.println("s1.equals(s4): "+s1.equals(s4));
+        System.out.println("\n");
+        System.out.println("(s1==s2): "+(s1==s2));
+        System.out.println("(s1==s3): "+(s1==s3));
+        System.out.println("(s1==s4): "+(s1==s4));
+        System.out.println("\n");
+        System.out.println("(s1.compareTo(s5): "+(s1.compareTo(s5)));
+        System.out.println("(s5.compareTo(s1): "+(s5.compareTo(s1)));
+        System.out.println("(s1.compareTo(s2): "+(s1.compareTo(s2)));
+        System.out.println("(s1.compareTo(s6): "+(s1.compareTo(s6)));
+        System.out.println("(s1.compareTo(s1): "+(s1.compareTo(s1)));
+        System.out.println("(s1.compareTo(s2): "+(s1.compareToIgnoreCase(s2)));
+
+
+    }
+}
